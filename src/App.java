de78463e21e50867f620dbd76d33083bdce80483
Smila M <136483240+smilanto@users.noreply.github.com>
2024-05-28 18:19:29 +0530

    Delete src directory

--- src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        // instatiate the GUI
-        PasswordGeneratorGUI passwordGeneratorGUI = new PasswordGeneratorGUI();
-
-        // make the GUI visible
-        passwordGeneratorGUI.setVisible(true);
-    }
-}
